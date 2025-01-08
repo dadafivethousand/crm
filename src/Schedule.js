0f0ae66d56b8f29ad7c223d084d5182962354c04
maxim <dada@gmail.com>
2025-01-07 23:56:40 -0500

    hi

--- src/Schedule.js
+++ src/Schedule.js
@@ -10,17 +10,21 @@ export default function Schedule() {
             <h1>Wednesday</h1>
 
             <div className='weekday morning'>  
-            <h4>7:30AM  BJJ NOGI</h4>
+            <h4>7:30AM - BJJ NOGI</h4>
             </div>
+
+            <hr />
 
             <div className='weekday lunch'>  
-            <h4>12:00PM  BJJ NOGI</h4>
+            <h4>12:00PM - BJJ NOGI</h4>
             </div>
 
+
+            <hr />
             <div className='weekday evening'>  
-            <h4>5:15PM  MMA</h4>
-            <h4>6:15PM  WRESTLING</h4>
-            <h4>7:15AM  BJJ NOGI</h4>
+            <h4>5:15PM - MMA</h4>
+            <h4>6:15PM - WRESTLING</h4>
+            <h4>7:15AM - BJJ NOGI</h4>
             </div>
 
 
